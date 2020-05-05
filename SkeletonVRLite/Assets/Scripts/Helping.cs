9c95ac7c7e6ca9f4812bbc60c38f04c39571aeba
danijb <d.boges@hotmail.com>
2020-05-05 16:45:50 +0300

    commiting project lite

---
+++ SkeletonVRLite/Assets/Scripts/Helping.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1011106b0bfb37e8e4391f3590ae5bc332223adbfccd26e162b03578dfdecd53
+size 855
---
+++ SkeletonVRLite/Assets/Scripts/Skeleton_Read.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a2bfbebf1deb518ba7484455087b80523d4374f3e018e98b858d9b87bc9575cc
+size 18058
